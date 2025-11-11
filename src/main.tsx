96f2f5e6d14c2ed9edc1e58dddc9978262908e84
ablacs <arthurblcs@yahoo.com.br>
2025-11-11 20:26:51 -0300

    ignoring the import app from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+//import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Torteria } from "./pages/Home/index.tsx";
 import { GlobalStyle } from "./styles.ts";
